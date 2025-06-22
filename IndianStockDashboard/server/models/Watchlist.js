cd86495132632fa0421eaca61e1a481391bcd26e
Nehal Ganesh Kulloli <nehalgk87@gmail.com>
2025-06-22 19:30:21 +0530

    Files upload

--- IndianStockDashboard/server/models/Watchlist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const WatchlistSchema = new mongoose.Schema({
-  symbol: { type: String, required: true },
-  addedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Watchlist", WatchlistSchema);
